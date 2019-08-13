b3a82ead3f769b9f79925d56f83dfa43242f6522
Angel Munoz <angelmsanchez85@gmail.com>
2019-08-13 17:58:34 +0200

    feching data from pages

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,39 @@
 import Link from 'next/link';
 
+import axios from 'axios';
+
 import Layout from '../src/components/Layout';
 
 const PostLink = props => (
-  <li>
-    <Link href="/post/[id]" as={`/post/${props.id}`}>
-      <a>{props.id}</a>
+  <li key={props.show.id}>
+    <Link href="/post/[id]" as={`/post/${props.show.id}`}>
+      <a>{props.show.name}</a>
     </Link>
   </li>
 );
 
-const Index = () => (
+const Index = props => (
   <Layout>
-    <p>Hello Next.js</p>
+    <h1>Batman TV Shows</h1>
     <ul>
-      <PostLink id="hello-nextjs" />
-      <PostLink id="learn-nextjs" />
-      <PostLink id="deploy-apps" />
+      {props.shows.map(show => (
+        <div>
+          <PostLink show={show} />
+        </div>
+      ))}
     </ul>
   </Layout>
-)
+);
+
+Index.getInitialProps = async function () {
+  const res = await axios.get('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.data;
+
+  console.log(`Show data fetched. Count:`, data.map(entry => entry.show));
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
 
 export default Index
--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,15 +1,22 @@
-import { useRouter } from 'next/router';
-import Layout from './../../src/components/Layout';
+import Layout from '../../src/components/Layout';
+import axios from 'axios';
 
-const Post = () => {
-  const router = useRouter();
+const Post = props => (
+  <Layout>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+    <img src={props.show.image.medium} />
+  </Layout>
+);
 
-  return (
-    <Layout>
-      <h1>{router.query.id}</h1>
-      <p>This is the blog post content.</p>
-    </Layout>
-  );
+Post.getInitialProps = async function(context) {
+  const { id } = context.query;
+  const res = await axios.get(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.data;
+
+  console.log(`Fetched show: ${show.name}`);
+
+  return { show };
 };
 
 export default Post;
