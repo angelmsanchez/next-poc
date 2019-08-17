0d3a408a3510eba507ed880bf26bd40c8e2eb709
Angel Munoz <angelmsanchez85@gmail.com>
2019-08-17 19:55:54 +0200

    sync props to state properly with react hooks

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,28 @@
+import { useState } from 'react';
+
 import Layout from '../src/components/Layout';
+import Profile from '../src/components/Profile';
 
 export default function About() {
+  const [state, setState] = useState({
+    name: 'Param',
+    email: 'param@gmail.com',
+  });
+
+  const handleChange = () => {
+    setState({
+      name: 'Vennila',
+      email: 'vennila@gmail.com',
+    });
+  };
+
   return (
     <Layout>
-      <p>About Page</p>
+      <p>About Page:</p>
+      <div>
+        <Profile {...state} />
+        <button onClick={handleChange}>Change Profile</button>
+      </div>
     </Layout>
   );
 }
---
+++ src/components/Profile.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+const Profile = props => {
+  const [profileState, setProfileState] = useState(props);
+
+  useEffect(() => {
+    setProfileState(props);
+  }, [props]);
+
+  return (
+    <div>
+      <p>
+        <strong>Name:</strong>
+        {profileState.name}
+      </p>
+      <p>
+        <strong>Email:</strong>
+        {profileState.email}
+      </p>
+    </div>
+  );
+};
+
+export default Profile;
